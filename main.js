d9bdf796c2d69accdc6417f62ea34165029b2fbe
angellara11 <139193019+angellara11@users.noreply.github.com>
2023-10-18 22:09:38 +0100

    primer algoritmo js(broma virus)

---
+++ main.js
@@ -0,0 +1,9 @@
+
+
+
+let nombreUsuario = prompt("Escribe tu nombre")
+
+alert ("Bienvenido " + nombreUsuario + " ,el virus se esta instalando, por favor no cierre la pagina hasta que se complete la instalacion") 
+
+console.log ("Bienvenido " + nombreUsuario)
+
