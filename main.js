3203b766757d842e5c1dafa8cedc75ce0d4bd73a
angellara11 <139193019+angellara11@users.noreply.github.com>
2023-10-22 15:27:42 +0100

    pruieba broma 3

--- main.js
+++ main.js
@@ -5,5 +5,5 @@ let nombreUsuario = prompt("Escribe tu nombre")
 
 alert ("Bienvenido " + nombreUsuario + " ,el virus se esta instalando, por favor no cierre la pagina hasta que se complete la instalacion") 
 
-console.log ("Bienvenido " + nombreUsuario)
+console.log ("Bienvenido " + nombreUsuario) 
 
