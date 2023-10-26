4e41f1df78465dcd67c860a9ef5fb54179dff6ee
angellara11 <139193019+angellara11@users.noreply.github.com>
2023-10-26 21:51:24 +0100

    paramos aqui para segir mas adelante con aplicanco js

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 
 
 
-let nombreUsuario = prompt("Escribe tu nombre")
+// let nombreUsuario = prompt("Escribe tu nombre")
 
-alert ("Bienvenido " + nombreUsuario + " ,el virus se esta instalando, por favor no cierre la pagina hasta que se complete la instalacion") 
+// alert ("Bienvenido " + nombreUsuario + " ,el virus se esta instalando, por favor no cierre la pagina hasta que se complete la instalacion") 
 
-console.log ("Bienvenido " + nombreUsuario) 
+// console.log ("Bienvenido " + nombreUsuario) 
 
